0d36d326f775f9c09fdc40f6712098b7df8b5a9f
Pocket7878 <poketo7878@gmail.com>
2021-11-14 15:42:46 +0900

    feature: Retrieve port from environment variable $PORT

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, world!")
 	})
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":"+port, nil)
 }
