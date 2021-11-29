184c352941c9359885997c2264218ebcf211274a
Pocket7878 <poketo7878@gmail.com>
2021-11-29 13:56:16 +0900

    fix: Early return.

--- infrastructure/presentation/todo_handler.go
+++ infrastructure/presentation/todo_handler.go
@@ -12,37 +12,6 @@ import (
 	"github.com/pocket7878/spa_login_learning_backend/domain"
 )
 
-func TodosGet(u domain.UserUsecase, t domain.TodoUsecase) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		//Retrieve & Ensure User
-		provider, uid := extractProviderAndUID(c)
-		user, err := ensureUser(c.Request.Context(), u, provider, uid)
-
-		todos, err := t.GetTodos(c, user.ID)
-		if err != nil {
-			c.AbortWithError(500, err)
-		}
-		response := make([]map[string]string, 0)
-		for _, t := range todos {
-			response = append(response, map[string]string{
-				"id":          fmt.Sprint(t.ID),
-				"user_id":     fmt.Sprint(t.UserID),
-				"description": t.Description,
-			})
-		}
-
-		c.JSON(http.StatusOK, response)
-	}
-}
-
-type PostTodoData struct {
-	Description string `json:"description"`
-}
-
-type PostTodoInput struct {
-	Data PostTodoData `json:"todo"`
-}
-
 func extractProviderAndUID(ctx *gin.Context) (string, string) {
 	claims := ctx.Request.Context().Value(jwtmiddleware.ContextKey{}).(*josev2.UserContext)
 	subject := claims.RegisteredClaims.Subject
@@ -74,6 +43,42 @@ func ensureUser(ctx context.Context, u domain.UserUsecase, provider, uid string)
 	return result, nil
 }
 
+func TodosGet(u domain.UserUsecase, t domain.TodoUsecase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//Retrieve & Ensure User
+		provider, uid := extractProviderAndUID(c)
+		user, err := ensureUser(c.Request.Context(), u, provider, uid)
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+
+		todos, err := t.GetTodos(c, user.ID)
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+		response := make([]map[string]string, 0)
+		for _, t := range todos {
+			response = append(response, map[string]string{
+				"id":          fmt.Sprint(t.ID),
+				"user_id":     fmt.Sprint(t.UserID),
+				"description": t.Description,
+			})
+		}
+
+		c.JSON(http.StatusOK, response)
+	}
+}
+
+type PostTodoData struct {
+	Description string `json:"description"`
+}
+
+type PostTodoInput struct {
+	Data PostTodoData `json:"todo"`
+}
+
 func TodoPost(u domain.UserUsecase, t domain.TodoUsecase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//Retrieve & Ensure User
@@ -82,6 +87,7 @@ func TodoPost(u domain.UserUsecase, t domain.TodoUsecase) gin.HandlerFunc {
 
 		if err != nil {
 			c.AbortWithError(500, err)
+			return
 		}
 
 		var jsonInput PostTodoInput
