806b769bb53fde0cdca32a31319331c7c0563877
Pocket7878 <poketo7878@gmail.com>
2021-11-29 21:22:02 +0900

    feature: Allow non application/json OPTIONS request

--- infrastructure/middleware/json_request_only.go
+++ infrastructure/middleware/json_request_only.go
@@ -1,9 +1,18 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func RequireJsonRequestBodyOnly() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if c.Request.Method == http.MethodOptions {
+			c.Next()
+			return
+		}
+
 		if c.Request.Header.Get("Content-Type") != "application/json" {
 			c.AbortWithStatus(400)
 			return
